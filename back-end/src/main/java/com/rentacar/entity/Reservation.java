25c06356d45b430eb78e92f8bb691c040e0505f1
Pasindu ishan <pasindu98ishan@gmail.com>
2024-01-22 21:18:39 +0530

    vehicle and reservation entity

---
+++ back-end/src/main/java/com/rentacar/entity/Reservation.java
@@ -0,0 +1,37 @@
+package com.rentacar.entity;
+
+import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="reservation")
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "reservation_date",nullable = false)
+    private Date reservationDate;
+    @Column(name = "pickup_date", nullable = false)
+    private Date pickupDate;
+    @Column(name="return_date",nullable=false)
+    private Date returnDate;
+    @Column(name="no_of_days",nullable=false)
+    private int noOfDays;
+    @Column
+    private String cancellationDetails;
+
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @OneToOne(mappedBy = "reservation")
+    private Set<Vehicle> rentSet;
+}
