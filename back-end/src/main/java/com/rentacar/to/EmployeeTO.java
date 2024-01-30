aa3475dfa432bfbc4f7cde6233e05a9155319f37
Wishva98 <buddhiwishvanath123@gmail.com>
2024-01-30 09:53:53 +0530

    edit entities

--- back-end/src/main/java/com/rentacar/to/EmployeeTO.java
+++ back-end/src/main/java/com/rentacar/to/EmployeeTO.java
@@ -1,7 +1,21 @@
 package com.rentacar.to;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeTO implements Serializable {
+    private Integer id;
+    private String fullName;
+    private String address;
+    private String nic;
+    private String email;
+    private Set<String> contacts;
 
 }
